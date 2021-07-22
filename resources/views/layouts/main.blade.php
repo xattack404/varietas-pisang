fd66070c2219256d452ca45710a82c15143e14a1
muhammadfarhannurpratama <55246227+muhammadfarhannurpratama@users.noreply.github.com>
2021-07-22 07:12:07 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,7 +34,7 @@
             </div>
             <footer class="main-footer">
                 <div class="footer-left">
-                    Copyright &copy; 2020 <div class="bullet"></div> Design By <a href="#">Kelompok 5 TIF BWS</a>
+                    Copyright &copy; 2021 <div class="bullet"></div> Design By <a href="#">Kelompok 20 TIF BWS</a>
                 </div>
                 <div class="footer-right">
                     2.3.0
